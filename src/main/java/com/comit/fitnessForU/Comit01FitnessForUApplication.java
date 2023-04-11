e3fa174667cb7af2575faeea09fb7c6af34ac486
lakshmitadepalli <post2deepu6@gmail.com>
2023-04-11 15:45:52 -0600

    first commit

---
+++ src/main/java/com/comit/fitnessForU/Comit01FitnessForUApplication.java
@@ -0,0 +1,13 @@
+package com.comit.fitnessForU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comit01FitnessForUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comit01FitnessForUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comit/fitnessForU/Comit01FitnessForUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comit.fitnessForU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comit01FitnessForUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
